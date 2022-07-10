8f5e2d0339c82ebaa7d28e31afa8af52613a21d7
jekim <jislji92@gmail.com>
2022-07-10 16:19:50 +0900

    add Service package

---
+++ src/main/java/com/ib/cat/service/ContentsUtil.java
@@ -0,0 +1,12 @@
+package com.ib.cat.service;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ContentsUtil {
+
+    private final String KEY="e8b937d602d4a3d3bda9a5be4c9a6a44";
+    //API를 통해 추출한 json 형태의 결과값을 저장할 변수
+    private String result ="";
+
+}
