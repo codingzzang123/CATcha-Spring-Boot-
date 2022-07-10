2df484e0d1a09fe37de90b9092b74ee28c5baf0f
jiseo1 <jiseong1028@naver.com>
2022-07-10 17:22:38 +0900

    Entity 이름 변경

--- src/main/java/com/ib/cat/controller/member/SignController.java
+++ src/main/java/com/ib/cat/controller/member/SignController.java
@@ -1,7 +1,15 @@
 package com.ib.cat.controller.member;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SignController {
+
+    @GetMapping("/member/sign")
+    public String getSign(){
+        return "member/sign";
+    }
+
+
 }
