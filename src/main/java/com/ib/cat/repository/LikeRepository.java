996dcea7d5b4c499d74f919bb711fdc4cdb86033
jekim <jislji92@gmail.com>
2022-07-13 18:58:01 +0900

    develop 오류 수정

--- src/main/java/com/ib/cat/repository/LikeRepository.java
+++ src/main/java/com/ib/cat/repository/LikeRepository.java
@@ -4,13 +4,15 @@ import com.ib.cat.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface LikeRepository extends JpaRepository<Like,Long> {
 
     //CRUD!
-
-    Like findByContentsNumAndCode(int contentsNum, int code);
     Like findByNameAndContentsNum(String name, int contentsNum);
+    Like findByNameAndContentsNumAndCode(String name, int contentsNum, int code);
 
-//    public int find
+    /* info -> memberlike */
+    List<Like> findAllByNameAndCode(String memberId, int code);
 
 }
