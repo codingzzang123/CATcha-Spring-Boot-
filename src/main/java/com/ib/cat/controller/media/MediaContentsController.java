4b711747a52f7b180462d22198f5118171571833
jekim <jislji92@gmail.com>
2022-07-19 19:49:17 +0900

    Contents 상세페이지 - 추천 장르 추가 (자기자신 추천 빼야함)

--- src/main/java/com/ib/cat/controller/media/MediaContentsController.java
+++ src/main/java/com/ib/cat/controller/media/MediaContentsController.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -33,9 +34,8 @@ public class MediaContentsController {
     @RequestMapping(value="/{type}/content/{contentsNum}", method= RequestMethod.GET)
     public ModelAndView detail(Model model,
                                @PathVariable(value="type") String contentsType,
-                               @PathVariable("contentsNum") int contentsNum //@RequestParam(value="userId") String userId
+                               @PathVariable("contentsNum") int contentsNum
                                ) { //HttpSession session,
-        System.out.println("contentsController 작동중");
 
         model.addAttribute("contentsNum", contentsNum);
         //contentsNum(id) 컨텐츠 Dto 가져옴
@@ -44,6 +44,7 @@ public class MediaContentsController {
 //        List<String> imageList = contentsService.getImages(contentsType, contentsNum);
         List<CreditsDto> cast = contentsService.getCredits(contentsType, contentsNum, "cast");
 //        List<CreditsDto> crew = contentsService.getCredits(contentsType, contentsNum, "crew");
+        List<ContentsDto> reco = new ArrayList<ContentsDto>();
 
         /*  view 경로 분기  */
         int code=0;
@@ -68,8 +69,43 @@ public class MediaContentsController {
 
         /*  OTT 찾기  */
         List<OttDto> ol = contentsService.getOTT(contentsType, contentsNum);
-        System.out.println("controller-list: "+ol);
         mav.addObject("ott", ol);
+
+        /*  추천 장르  */
+
+        List<ContentsDto> temp = new ArrayList<ContentsDto>();
+        temp = contentsService.getInfoList(contentsType); //임시 리스트에 모든 컨텐츠 저장
+
+        for (int i = 0 ; i < temp.size() ; i++) {
+            List<Integer> list1 = new ArrayList<>(temp.get(i).getGenres()); //전체
+//            System.out.println("controller - list1.size: "+ list1.size());
+            List<Integer> list2 = new ArrayList<>(contents.getGenres()); //target
+//            System.out.println("controller - list2: " + list2);
+
+            if(list2.size() == 1) { //target-등록 장르 1개일때
+                list1.retainAll(list2); //list1, list2 공통 요소만 list1에 남김
+                if (list1.size() == 1) {
+                    ContentsDto dto = new ContentsDto();
+                    dto = temp.get(i);
+                    reco.add(dto);
+                }
+            } else if (list2.size() == 2) {
+                list1.retainAll(list2);
+                if(list1.size() == 2) {
+                    ContentsDto dto = new ContentsDto();
+                    dto = temp.get(i);
+                    reco.add(dto);
+                }
+            } else {
+                list1.retainAll(list2);
+                if(list1.size() >= 3) {
+                    ContentsDto dto = new ContentsDto();
+                    dto = temp.get(i);
+                    reco.add(dto);
+                }
+            }
+        }
+        mav.addObject("reco", reco);
 
         return mav;
     }
--- src/main/java/com/ib/cat/dto/media/ContentsDto.java
+++ src/main/java/com/ib/cat/dto/media/ContentsDto.java
@@ -8,32 +8,31 @@ import java.util.List;
 
 @Data
 public class ContentsDto {
-    private int contentsNum;//v
-    private String contentsType;//v
-    private String title;//v
-    private String overview;//v
+    private int contentsNum;//컨텐츠 id
+    private String contentsType;//movie, tv
+    private String title;//
+    private String overview;//
 
     @Column(columnDefinition = "default 'cinema_default.png'")
-    private String posterPath;//v
-    private Date releaseDate;//v
-    private float voteAverage;//v
-    private float popularity;//v
+    private String posterPath;//포스터 사진
+    private Date releaseDate;//공개일
+    private float voteAverage;//평점
+    private float popularity;//인기
     private String genre; //컨텐츠 장르
-    private List<Integer> genres;//v
+    private List<Integer> genres;//컨텐츠 장르
 
-    private String runtime;
+    private String runtime;//런타임
     private int hour;
     private int minute;
-
 
     private int count;
     private Double star; //평점
     //	private GenresVO genresVO;
     /**/
     private List<GenresDto> ls;
-
-    private int totalResults;
-    private int totalPages;
-    private int page;
+//
+//    private int totalResults;
+//    private int totalPages;
+//    private int page;
 
 }
